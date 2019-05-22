2537935c81071c1bd3e49ec3cd58beb174d80f99
tl.ni <tl.ni@ifootstone.com>
2019-05-22 21:41:24 +0800

    20190522springboot项目配置多环境以及测试jenkins

---
+++ src/main/java/com/longe/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.longe.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tianl on 2019/5/22.
+ */
+@RestController
+public class IndexController {
+
+    @Value("${env}")
+    private String ENV;
+
+    @RequestMapping("/index")
+    public String index(){
+
+        return "此时的环境为:" + ENV;
+    }
+}
